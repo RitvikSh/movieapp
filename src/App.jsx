f2e04d7e6b6a0d325b12ead117a9cd1f9db3988b
RitvikSh <praveensiarma@gmail.com>
2023-08-27 11:02:50 +0530

    basic user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import './App.css'
+import { useEffect } from 'react';
+import searchbar from "./searchbar.svg"
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=232b3ad4"
+
+const App = () => {
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.search);
+  }
+  useEffect(() => {
+    searchMovie("superman");
+  }, []);
 
   return (
-    <>
-      <h1>Movie streaming app</h1>
-    </>
-  )
+    <div className='App'>
+      <h1>MovieBase</h1>
+
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value="Superman"
+          onChange={() => { }}
+        />
+        <img src={searchbar} alt="search" onClick={() => { }} />
+      </div>
+      <div className='container'>
+
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
